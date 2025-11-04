4f73c793635f93e77d2531a92e10c30da202be46
csolanoave <csolano@asertec.com.ec>
2025-11-04 06:39:37 -0500

    ServiceImpl

--- src/main/java/com/springboot/backend/andres/usersapp/usersbackend/services/UserServiceImpl.java
+++ src/main/java/com/springboot/backend/andres/usersapp/usersbackend/services/UserServiceImpl.java
@@ -1,8 +1,44 @@
 package com.springboot.backend.andres.usersapp.usersbackend.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.backend.andres.usersapp.usersbackend.entities.User;
+import com.springboot.backend.andres.usersapp.usersbackend.repositories.UserRepository;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(@NonNull Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public User save(@NonNull User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(@NonNull Long id) {
+        userRepository.deleteById(id);
+    }
 
 }
